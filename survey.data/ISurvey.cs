f9480dc7c5802977202dcec2496eb4f3c198b916
Georgi Kamacharov <kamacharov.gk@pg.com>
2019-06-28 14:56:23 -0400

    add survey object

---
+++ survey.data/ISurvey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace survey.data
+{
+    public interface ISurvey
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        Guid PublicKey { get; set; }
+        ICollection<Question> Questions { get; set; }
+    }
+}
---
+++ survey.data/Survey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace survey.data
+{
+    public class Survey : ISurvey, IPublicKeyId
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("public_key")]
+        public Guid PublicKey { get; set; }
+
+        [JsonProperty("questions")]
+        public ICollection<Question> Questions { get; set; } = new List<Question>();
+    }
+}
