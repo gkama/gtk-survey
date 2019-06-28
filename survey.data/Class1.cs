29fce7395471871abf1b06b1c7159ba3d219704c
Georgi Kamacharov <kamacharov.gk@pg.com>
2019-06-28 10:06:52 -0400

    add initial nuget packages and classes

--- survey.data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace survey.data
-{
-    public class Class1
-    {
-    }
-}
---
+++ survey.data/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace survey.data
+{
+    public class Question
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
