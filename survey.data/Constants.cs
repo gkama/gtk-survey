21100bde0f94d182298dd369bb86e60907b88058
Georgi Kamacharov <kamacharov.gk@pg.com>
2019-07-01 14:52:43 -0400

    add constants and extension method

---
+++ survey.data/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace survey.data
+{
+    public static class Constants
+    {
+        public static int DefaultCacheSettings => 60;
+    }
+}
---
+++ survey.data/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Caching.Memory;
+
+namespace survey.data
+{
+    public static class ExtensionMethods
+    {
+        public static T GetObjectById<T>(this IMemoryCache Cache, IQueryable<T> Query, string Key, int Id)
+            where T : IPublicKeyId
+        {
+            return Cache.GetOrCreate<T>(Key, c =>
+            {
+                c.SlidingExpiration = TimeSpan.FromSeconds(Constants.DefaultCacheSettings);
+
+                return Query
+                    .FirstOrDefault<T>(x => x.Id == Id);
+            });
+        }
+    }
+}
