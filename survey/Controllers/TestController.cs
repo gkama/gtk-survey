87e4c508841d307b29e086c6a1b80452c8e26c54
Georgi Kamacharov <kamacharov.gk@pg.com>
2019-08-14 10:51:13 -0400

    add test controller

---
+++ survey/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using survey.data;
+using survey.services;
+
+namespace survey.Controllers
+{
+    [Route("test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public readonly ISurveyRepository repo;
+
+        public TestController(ISurveyRepository repo)
+        {
+            this.repo = repo;
+        }
+    }
+}
