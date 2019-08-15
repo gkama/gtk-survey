b3abe8b741d23f8ecf9d3731b489d56e71e450d2
Georgi Kamacharov <kamacharov.gk@pg.com>
2019-08-15 14:57:33 -0400

    add workspace class

---
+++ survey.data/Workspace.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace survey.data
+{
+    public class Workspace : IPublicKeyId
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("slug")]
+        public string Slug { get; set; }
+
+        [JsonProperty("public_key")]
+        public Guid PublicKey { get; set; }
+    }
+}
