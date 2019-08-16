367440b75bf5e827781d1965e6d365c3b85b1f5e
Georgi Kamacharov <gkamacharov@gmail.com>
2019-08-16 17:39:25 -0400

    add workspace gtype for graphql

---
+++ survey.data/WorkspaceGType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using GraphQL.Types;
+
+namespace survey.data
+{
+    public class WorkspaceGType : ObjectGraphType<Workspace>
+    {
+        public WorkspaceGType()
+        {
+            Field(x => x.Id);
+            Field(x => x.Name);
+            Field(x => x.Slug);
+            Field(x => x.PublicKey, type: typeof(IdGraphType));
+        }
+    }
+}
