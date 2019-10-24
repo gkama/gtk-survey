09b4c6b4a632b8a2bf74d96408af8d0a3e94251e
Georgi Kamacharov <kamacharov.gk@pg.com>
2019-10-24 12:47:39 -0400

    add survey category class

---
+++ survey.data/SurveyCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace survey.data
+{
+    public class SurveyCategory : IPublicKeyId
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("nameId")]
+        public string NameId => this.Name.Trim().ToLower();
+
+        [JsonProperty("publicKey")]
+        public Guid PublicKey { get; set; }
+    }
+}
