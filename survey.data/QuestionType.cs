a410d11d9d825cfc0f43cd97ea4297a6874580fd
Georgi Kamacharov <kamacharov.gk@pg.com>
2019-10-24 12:41:16 -0400

    update question type to include a name id which is just the name trimmed and lower cased

--- survey.data/QuestionType.cs
+++ survey.data/QuestionType.cs
@@ -6,11 +6,18 @@ using Newtonsoft.Json;
 
 namespace survey.data
 {
-    //public int OpenEnded;       //Open ended answers
-    //public int MultipleChoice;  //Multiple choice defined by the user
-    //public int YesNo;           //Yes or No
-    //public int Semantic;        //On a scale of 1 to 10 (max is 1 to 10)
-    //public int Likert;          //Strongly agree, Agree, Neither agree nor disagree, Disagree, Strongly disagree
+    /*
+     * public int OpenEnded;       //Open ended answers
+     * public int MultipleChoice;  //Multiple choice defined by the user
+     * public int YesNo;           //Yes or No
+     * public int Semantic;        //On a scale of 1 to 10 (max is 1 to 10)
+     * public int Likert;          //Strongly agree, Agree, Neither agree nor disagree, Disagree, Strongly disagree
+     * 
+     * Check Boxes
+     * Star Rating
+     * Single Textbox
+     * Date / Time
+     */
 
     public class QuestionType : IQuestionType, IPublicKeyId
     {
@@ -19,6 +26,9 @@ namespace survey.data
 
         [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("nameId")]
+        public string NameId => this.Name.Trim().ToLower();
 
         [JsonProperty("publicKey")]
         public Guid PublicKey { get; set; }
