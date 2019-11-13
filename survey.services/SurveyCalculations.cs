c5d5c2f2f8083cb411c50056a274be719cde4b60
Georgi Kamacharov <gkamacharov@gmail.com>
2019-11-13 18:29:39 -0500

    add survey calculations class and interface shell

---
+++ survey.services/SurveyCalculations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace survey.services
+{
+    public class SurveyCalculations : ISurveyCalculations
+    {
+        public readonly ISurveyRepository SurveyRepository;
+
+        public SurveyCalculations(ISurveyRepository SurveyRepository)
+        {
+            this.SurveyRepository = SurveyRepository;
+        }
+    }
+}
