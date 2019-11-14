99aed0bbf546f53927b50fbcff680963af2a6506
Georgi Kamacharov <gkamacharov@gmail.com>
2019-11-13 21:00:01 -0500

    add shell of get average surveys method

--- survey.services/ISurveyRepository.cs
+++ survey.services/ISurveyRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace survey.services
 {
     public interface ISurveyRepository
     {
+        IQueryable<Client> GetClientsQuery();
         IEnumerable<Client> GetClients();
         Task<Client> GetClientAsync(int Id);
         Task<Client> GetClientAsync(Guid PublicKey);
--- survey.services/SurveyCalculations.cs
+++ survey.services/SurveyCalculations.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 using Microsoft.Extensions.Logging;
 
@@ -15,6 +15,15 @@ namespace survey.services
         {
             this.log = log;
             this.repo = repo;
+        }
+
+        public void GetAverageSurveysAsync(DateTime Date)
+        {
+            var surveys = repo.GetClientsQuery()
+                .AsEnumerable()
+                .Where(x => x.LastUpdated <= Date);
+
+            var counts = surveys.Count();
         }
     }
 }
