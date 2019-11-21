959eb4b78f600c7f68c1f09617f582a20bab0316
Georgi Kamacharov <gkamacharov@gmail.com>
2019-11-20 23:35:59 -0500

    move http method on top of route

--- survey/Controllers/SurveyController.cs
+++ survey/Controllers/SurveyController.cs
@@ -19,43 +19,43 @@ namespace survey.Controllers
             this.repo = repo;
         }
 
-        [Route("all")]
         [HttpGet]
+        [Route("all")]
         public IActionResult Get()
         {
             return Ok(repo.GetSurveys());
         }
 
-        [Route("{id}")]
         [HttpGet]
+        [Route("{id}")]
         public async Task<IActionResult> GetSurveyAsync([FromRoute]int id)
         {
             return Ok(await repo.GetSurveyAsync(id));
         }
 
-        [Route("categories")]
         [HttpGet]
+        [Route("categories")]
         public IActionResult GetSurveyCategories()
         {
             return Ok(repo.GetSurveyCategories());
         }
 
-        [Route("category/{id}")]
         [HttpGet]
+        [Route("category/{id}")]
         public async Task<IActionResult> GetSurveyCategoryAsync([FromRoute]int id)
         {
             return Ok(await repo.GetSurveyCategoryAsync(id));
         }
 
-        [Route("category/{name}")]
         [HttpGet]
+        [Route("category/{name}")]
         public async Task<IActionResult> GetSurveyCategoryAsync([FromRoute]string name)
         {
             return Ok(await repo.GetSurveyCategoryAsync(name));
         }
 
-        [Route("category/add/{name}")]
         [HttpGet]
+        [Route("category/add/{name}")]
         public async Task<IActionResult> AddSurveyCategoryAsync([FromRoute]string name)
         {
             await repo.AddSurveyCategoryAsync(name);
@@ -63,71 +63,71 @@ namespace survey.Controllers
             return Ok("success");
         }
 
-        [Route("{id}/questions")]
         [HttpGet]
+        [Route("{id}/questions")]
         public async Task<IActionResult> GetSurveyQuestionsAsync([FromRoute]int id)
         {
             return Ok(await repo.GetSurveyQuestionsAsync(id));
         }
 
-        [Route("publickey/{publickey}")]
         [HttpGet]
+        [Route("publickey/{publickey}")]
         public async Task<IActionResult> GetSurvey([FromRoute]Guid publickey)
         {
             return Ok(await repo.GetSurveyAsync(publickey));
         }
 
-        [Route("responses")]
         [HttpGet]
+        [Route("responses")]
         public IActionResult GetResponses()
         {
             return Ok(repo.GetResponses());
         }
 
-        [Route("{id}/responses")]
         [HttpGet]
+        [Route("{id}/responses")]
         public async Task<IActionResult> GetResponsesCustomBySurveyIdAsync([FromRoute]int id)
         {
             return Ok(await repo.GetResponsesStatsAsync(id));
         }
 
-        [Route("{id}/responses/count")]
         [HttpGet]
+        [Route("{id}/responses/count")]
         public async Task<IActionResult> GetResponsesCountAsync([FromRoute]int id)
         {
             return Ok(await repo.GetResponsesCountAsync(id));
         }
 
-        [Route("{id}/responses/stats")]
         [HttpGet]
+        [Route("{id}/responses/stats")]
         public async Task<IActionResult> GetResponsesStatsAsync([FromRoute]int id)
         {
             return Ok(await repo.GetResponsesStatsAsync(id));
         }
 
-        [Route("create")]
         [HttpPost]
+        [Route("create")]
         public async Task<IActionResult> CreateSurveyAsync([FromBody]SurveyQuestionRequest request)
         {
             return Ok(await repo.CreateSurveyAsync(request.SurveyName, request.QuestionRequests));
         }
 
-        [Route("{id}/response/update/{questionid}/{answer}")]
         [HttpGet]
+        [Route("{id}/response/update/{questionid}/{answer}")]
         public async Task<IActionResult> UpdateResponseAsync([FromRoute]int id, int questionid, string answer)
         {
             return Ok(await repo.UpdateResponseAsync(id, questionid, answer));
         }
 
-        [Route("today")]
         [HttpGet]
+        [Route("today")]
         public async Task<IActionResult> GetSurveysTodayAsync()
         {
             return Ok(await repo.GetSurveysToday());
         }
 
-        [Route("today/count")]
         [HttpGet]
+        [Route("today/count")]
         public async Task<IActionResult> GetSurveysTodayCountAsync()
         {
             return Ok(await repo.GetSurveysTodayCount());
