a07407d4e4b813d5bb8e03a3ef07fd910fb17523
Georgi Kamacharov <gkamacharov@gmail.com>
2019-11-26 10:54:26 -0500

    add enum for question types

--- survey.data/Constants.cs
+++ survey.data/Constants.cs
@@ -7,5 +7,13 @@ namespace survey.data
     public static class Constants
     {
         public static int DefaultCacheSettings => 60;
+
+        public enum QuestionTypes
+        {
+            YesNo = 1,
+            Semantic = 2,
+            OpenEnded = 3,
+            Likert = 4
+        }
     }
 }
