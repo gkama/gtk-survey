efc4f0e490c1cff99576652d6e180997e13c0ecd
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-14 15:09:08 -0500

    update survey interface

--- survey.data/ISurvey.cs
+++ survey.data/ISurvey.cs
@@ -12,7 +12,11 @@ namespace survey.data
         string CreatedBy { get; set; }
         DateTime LastUpdated { get; set; }
         string LastUpdatedBy { get; set; }
+        int? CategoryId { get; set; }
+        SurveyCategory Category { get; set; }
         int? WorkspaceId { get; set; }
+        Workspace Workspace { get; set; }
         Guid PublicKey { get; set; }
+        ICollection<SurveyQuestion> SurveyQuestions { get; }
     }
 }
